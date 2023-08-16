b208a91716954e56d082bed36d651b9a3c8966c3
lau-phu <123408019+lau-phu@users.noreply.github.com>
2023-08-15 20:25:55 -0400

    Create AIgenerated.py

---
+++ AIgenerated.py
@@ -0,0 +1,4 @@
+#this gonna be a complete AI generated game. 
+#Chat GPT will generate all the mechanic and game funtion ideas
+#art can be made in stable diffusion 
+#since i will be making game from scrath, dungeon crawler RPG will be enough
